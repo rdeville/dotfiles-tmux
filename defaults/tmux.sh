55e22c70064546e308cb4074cd2a0f31ca48f3b7
Romain Deville <code@romaindeville.fr>
2024-03-28 16:28:09 +0100

    ⚡️ Improve tmux pane config

--- defaults/tmux.sh
+++ defaults/tmux.sh
@@ -19,33 +19,27 @@ tmux set -g window-status-separator ""
 tmux setw -g window-status-format "#(#{window} 'normal')"
 tmux setw -g window-status-current-format "#(#{window} 'current')"
 
-# Set color of active and inactive windows/pane
-tmux set -g window-style "fg=${grey_500},bg=default"
-tmux set -g window-active-style "fg=${grey_100},bg=default"
 # Set color and style of pane border
 tmux set -g pane-border-lines double
-tmux set -g pane-border-style "fg=${grey_500},bg=default"
-tmux set -g pane-active-border-style "fg=${green_500},bg=default"
+tmux set -g pane-border-style "fg=#222222,bg=#222222"
+tmux set -g pane-active-border-style "fg=${green_900},bg=${green_900}"
 tmux set -g pane-border-indicators off
 # Only show pane status if more than one pane
 tmux set-hook -g -w pane-focus-in "set-option -Fw pane-border-status '#{?#{e|>:#{window_panes},1},top,off}'"
 
+inactive_pane="#[bg=#111111]#[fg=#FFFFFF]"
+active_pane="#[fg=${white}]#[bg=${green_500}]"
+test_pane_first_powerline="#{?#{pane_active},#[bg=${green_500}]#[fg=${green_900}],#[fg=#222222]#[bg=#111111]}"
+test_pane_last_powerline="#{?#{pane_active},#[bg=${green_900}]#[fg=${green_500}],#[fg=#111111]#[bg=#222222]}"
+test_pane="#{?#{pane_active},$active_pane,$inactive_pane}"
+
 if [[ "$(uname)" == "Darwin" ]]
 then
-  tmux set -g pane-border-format " #{pane_index} #(ps -t #{pane_tty} -o args= | head -n 1) "
+  ps_cmd="#(ps -t #{pane_tty} -o args= | head -n 1)"
 else
-  tmux set -g pane-border-format " #{pane_index} #(ps --no-headers -t #{pane_tty} -o args -o-c) "
+  ps_cmd="#(ps --no-headers -t #{pane_tty} -o args -O-c | sed -e 's|.*\(nvim\).*|\1|g' -e 's|^-||' )"
 fi
-tmux set -g pane-border-format " #{pane_index} #(ps -t #{pane_tty} -o args= | tail -1) "
-
-# tmux set-hook -g client-attached 'display-message "hi world"'
-# tmux set-hook -g client-attached 'osascript -e "display notification \"hello world!\""'
-# tmux set-hook -g after-client-session-changed "osascript -e 'display notification \"hello world!\"'"
-
-# KEY BINDING
-# -----------------------------------------------------------------------------
-tmux set -g bind-key C-Tab next-window
-tmux set -g bind-key C-S-Tab previous-window
+tmux set -g pane-border-format "${test_pane_first_powerline}${test_pane} #{pane_index} ${ps_cmd} ${test_pane_last_powerline}"
 
 # SSH toggle nested tmux key binding
 # -----------------------------------------------------------------------------
@@ -57,13 +51,17 @@ tmux set -g bind-key C-S-Tab previous-window
 # See: toggle on/off all keybindings · Issue #237 · tmux/tmux -
 #   https://github.com/tmux/tmux/issues/237
 
+
+# set status-style 'fg=${status[fg]} strikethrough,bg=${status[bg]}'
+# set status-left ''
+# set status-right ''
 # Also, change some visual styles when window keys are off
 tmux bind -T root F12 "
   set prefix None
   set prefix2 None
   set key-table off
   set status-position top
-  set status-style 'fg=${status[fg]} strikethrough,bg=${status[bg]}'
+  set status off
   if -F '#{pane_in_mode}' 'send-keys -X cancel'
   refresh-client -S"
 
@@ -72,5 +70,5 @@ tmux bind -T off F12 "
   set -u prefix2
   set -u key-table
   set status-position bottom
-  set -u status-style
+  set -u status
   refresh-client -S"
