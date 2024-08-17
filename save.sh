25dec544568ebd52245406fec176fa9771ddf9f2
Romain Deville <code@romaindeville.fr>
2024-08-17 15:58:47 +0200

    🔥 Remove useless files

--- save.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if tmux info &> /dev/null
-then
-  "${XDG_CONFIG_HOME:-${HOME}/.config}/tmux/plugins/tmux-resurrect/scripts/save.sh"
-else
-  echo "No Tmux Server" &>>/tmp/tmux_save.log
-  exit 1
-fi
