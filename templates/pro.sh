c43edbe38ac140bf3a6cf46e16543924d9701a5e
Romain Deville <code@romaindeville.fr>
2024-08-17 19:55:20 +0200

    🔧 Update server/pro/perso/mini_computer templates

---
+++ templates/pro.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154,SC2034
+
+status[bg]="${red_900}"
+status[fg]="${grey_100}"
+
+window[bg]="${red_700}"
+window[fg]="${red_400}"
+
+window[current_bg]="${red_a200}"
+window[current_fg]="${black}"
