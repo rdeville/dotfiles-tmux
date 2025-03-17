fd7eb83b32a535f36bea76732a51878f3cd3dd07
Romain Deville <code@romaindeville.fr>
2025-03-17 15:22:44 +0100

    ✨(templates): Add default templates, update existing once

--- templates/perso.sh
+++ templates/perso.sh
@@ -9,8 +9,3 @@ window[fg]="${green_700}"
 
 window[current_bg]="${green_a400}"
 window[current_fg]="${black}"
-
-status[left_module]="mode_indicator session git"
-status[right_module]="hostname date"
-
-date[format]="%a %d %b | %H:%M"
