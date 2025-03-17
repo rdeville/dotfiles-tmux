326bb91e830cbf842fcfefababf0f9f92ad60275
Romain Deville <code@romaindeville.fr>
2025-03-17 15:23:44 +0100

    🔧(hosts): Update hosts config

--- hosts/darth-maul.sh
+++ hosts/darth-maul.sh
@@ -1,16 +1,1 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2154,SC2034
-
-status[bg]="${green_700}"
-status[fg]="${grey_100}"
-
-window[bg]="${green_500}"
-window[fg]="${green_700}"
-
-window[current_bg]="${green_a400}"
-window[current_fg]="${black}"
-
-status[left_module]="mode_indicator session git"
-status[right_module]="hostname date"
-
-date[format]="%a %d %b | %H:%M"
+../templates/perso.sh
